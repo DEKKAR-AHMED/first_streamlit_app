d8e717b1cfc9c694e9a6a37be9f6d8fc7d96b756
MIDOU <36410248+DEKKAR-AHMED@users.noreply.github.com>
2024-01-30 16:56:16 +0100

    Update streamlit_app.py
    add some text and emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacoda Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
